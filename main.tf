b2efdb84c43991313bbb4533df1b098849f7dd27
Nargiza <r.nakosya@gmail.com>
2024-03-18 11:11:36 -0500

    changed workflow

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ provider "aws"{
   region = "us-east-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "${BUCKET_TF_STATE}"
-    key    = "terraform.tfstate"
-    region = "${AWS_REGION}"
-  }
-}
 resource "aws_ecr_repository" "foo" {
   name                 = "bar"
   image_tag_mutability = "IMMUTABLE"
