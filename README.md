4a3de3be5f367f4407c4a86381acb6af7694ff87
Nargiza <r.nakosya@gmail.com>
2024-03-18 09:56:49 -0500

    added ecr.tf file and github actions workflow

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # terraform-githubactions
+Write a GitHub Actions workflow for a terraform
---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+  region = "us-est-2"
+}
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
