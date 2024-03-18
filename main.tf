67466b71f7132b250acc56a9f13a3fd87d7ec93f
Nargiza <r.nakosya@gmail.com>
2024-03-18 17:45:13 -0500

    added s3

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket                  = "tfstatebucketnari"
+    key                     = "my-terraform-project"
+    region                  = "us-east-2"
+    # shared_credentials_file = "~/.aws/credentials"
+  }
+}
+
 provider "aws"{
   region = "us-east-2"
 }
